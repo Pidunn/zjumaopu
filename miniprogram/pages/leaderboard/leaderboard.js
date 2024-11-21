62e072ea6ce0ad874bd3e0e8dbcaeb70af38cb02
JukieChen <18758218395@163.com>
2024-11-21 09:07:11 +0800

    新增照片月榜 + 绝育标签筛选

--- miniprogram/pages/leaderboard/leaderboard.js
+++ miniprogram/pages/leaderboard/leaderboard.js
@@ -30,7 +30,7 @@ Page({
     tempPics: [],
     loadnomore: false,
     // threads: ["徽章收集", "照片点赞", "拍照月榜"],
-    threads: ["照片点赞"],
+    threads: ["照片点赞", "拍照月榜"],
     threadsActive: 0,
     filters: [{
       name: "周精选",
@@ -316,7 +316,7 @@ Page({
     if (index == 0) {
       this.selectComponent('#badge-rank').reloadData();
     }
-    if (index == 2) {
+    if (index == 1) {
       this.selectComponent('#photo-rank').reloadData();
     }
   },
